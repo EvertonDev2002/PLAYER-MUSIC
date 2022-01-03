3e43e74f21649102658c9b9efc5344e65b9c256e
EvertonDev2002 <foreverplayer95@gmail.com>
2022-01-03 13:41:06 -0300

    heroku

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://ex-player.herokuapp.com",
   headers: { "Content-Type": "application/json", Accept: "application/json" },
 });
 export default instance;
